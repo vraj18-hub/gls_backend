f68e4d909afa726dc96bf6340d7648e4e23ae2bb
vraj18-hub <khatrivraj18@gmail.com>
2026-07-11 13:58:51 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,17 @@
+let express = require('express');
+let app = express();
+let port = 9000;
+
+let cors = require('cors');
+
+app.use(cors());
+
+app.get('/ping', (req, res) => {
+    res.send({
+        message: "Hello, World!"
+    });
+});
+
+app.listen(port, () => {
+    console.log("Server is running on http://localhost:9000")
+});
